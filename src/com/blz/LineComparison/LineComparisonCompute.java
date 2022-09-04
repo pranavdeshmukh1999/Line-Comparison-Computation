89381fb2c562f9f8355712f0b9170405c48c8ac7
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-04 22:15:12 +0530

    Added:Welcome Message

---
+++ src/com/blz/LineComparison/LineComparisonCompute.java
@@ -0,0 +1,10 @@
+package com.blz.LineComparison;
+
+public class LineComparisonCompute {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Line Computation Comprison Program ");
+	}
+
+}
